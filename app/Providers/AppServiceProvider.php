89fa2e0d825b523ebeded117c4ba3b90f75a8e54
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2018-11-01 21:07:41 +0100

    View composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Tag;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,8 +13,17 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        //
+    {                    //a ovde da smo hteli samo na posts onda ovako ['posts.index', 'posts.show']   
+        view()->composer('layouts.master', function($view){ //ovde koristimo layouts.master jer nam je on svuda ekstendovan i svuda nam je vidljiv svuda
+             //prvi parametar je view a dgugi niz je callback funkcija
+            $tags = Tag::has('posts')->get();  //posts je relacija
+            //vrati mi tag koji je na nekom postu
+            //has nam proverava da li ima relaciju i ako ima onda mi daj sve koje ima
+
+            $view->with('tags', $tags);
+            //view prosledjujemo te tagove
+         });
+       
     }
 
     /**
